520b7a443f2d8a268ae277eb188546fcf3f2d393
Caio Tenório <caiomatenorio@gmail.com>
2025-06-19 09:28:05 -0300

    Initial commit

---
+++ src/main/java/com/keepflower/backend/KeepflowerApplication.java
@@ -0,0 +1,13 @@
+package com.keepflower.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepflowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepflowerApplication.class, args);
+    }
+
+}
